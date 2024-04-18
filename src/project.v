da377ad6e09920726fdd73ebfef8842211a2ff07
macros77 <pawel.bialic@gmail.com>
2024-04-18 20:51:18 +0200

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_macros77_bcd (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,9 +16,30 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  reg [11:0] bcd = 0;
+  reg [3:0] counter = 0;
+    
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_oe  = 1;
+  assign uo_out  = bcd[7:0];
+  assign uio_out [3:0] = bcd[11:8];  
+
+  assign uio_out [7:4] = counter;
+
+  integer i;
+	
+  always @(ui_in) begin
+    bcd=0;		 	
+      for (i=0;i<8;i=i+1) begin					
+        if (bcd[3:0] >= 5) bcd[3:0] = bcd[3:0] + 3;	
+    	if (bcd[7:4] >= 5) bcd[7:4] = bcd[7:4] + 3;
+    	if (bcd[11:8] >= 5) bcd[11:8] = bcd[11:8] + 3;
+        bcd = { bcd[10:0], ui_in[7-i] };		
+    end
+  end    
+
+  always @(posedge clk) begin
+      counter <= counter + 1;
+  end
 
 endmodule
